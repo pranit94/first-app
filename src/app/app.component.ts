95089fabfb2beb1beb940854efb595885e6eebb7
Pranit Roy <pranit@gromor.in>
2019-09-23 20:09:16 +0530

    first commit with basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
+/**
+ * Here app-root is the root component of the application and is registered in the index.html file.
+ * The angular modules simply load this component into the index.html file on when page is loaded.
+ * We can have multiple components in angular.
+ */
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'first-app';
+  title = "Pranit";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-
+import { AppComponent } from "./app.component";
+/**
+ * All global imports of the application are made here.
+ * Any imported module has to be added into the @NgModule 's imports subsection.
+ */
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  /**
+   * All the code written in an angular project is done in typescript which is a superset of javascript.
+   * Typescript is much more robust than javascript which allows us to check code type at the time of writing the code and hence
+   * it is much more safer than javascript. It uses much more features like classes, types and Interfaces rather than
+   * vanilla JS.Typescript does not run in the browser and is compiled to javascript in the end. Hence angular is used along with typescript.
+   *
+   */
+}
